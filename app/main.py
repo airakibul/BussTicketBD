d368591b157d1cdfea1cba80e7da131fc5f85e72
airakibul <rakibul.cse.ai@gmail.com>
2025-11-22 07:01:19 +0600

    data loder is done here

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 from app.api.routes.chat import chat_router
+from app.services.buss_data_loader import startup_event
 
 app = FastAPI()
 
 
 @app.on_event("startup")
-async def startup_event():
-    # Perform any startup initialization here
-    
-    pass
+async def _startup_event():
+    await startup_event()
 
 app.include_router(chat_router)
